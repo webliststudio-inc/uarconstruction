9e5b6a506f4dd90cf8b328a2fd4a1ab6f25b02de
Mike Afolabi <afolabi.o@eleadernetwork.com>
2026-06-09 17:38:45 +0100

    first commit

--- config/constants.php
+++ config/constants.php
@@ -1,20 +1,21 @@
 <?php
-	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
-	$websiteAutoUrl =(isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$appName='Urban and Rural Construction LLC'; 
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
+$websiteAutoUrl = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$appName = 'Urban and Rural Construction';
 
-	//$websiteUrl='https://uarconstruction.com'; /// For Live Server Url //
-	$websiteUrl='http://localhost/weblist-studio/uarconstruction.com';
-	//$websiteUrl='http://10.143.4.177/weblist-studio/uarconstruction.com';
-	//$websitePath = $_SERVER['DOCUMENT_ROOT'];
-	$websitePath = $_SERVER['DOCUMENT_ROOT'].'/weblist-studio/uarconstruction.com'; //dirname(__FILE__);
-	$codeVersion= date('Ymdhis');
+//$websiteUrl='https://uarconstruction.com'; /// For Live Server Url //
+$websiteUrl = 'http://localhost/weblist-studio/uarconstruction';
+//$websiteUrl='http://10.143.4.177/weblist-studio/uarconstruction';
+//$websitePath = $_SERVER['DOCUMENT_ROOT'];
+$websitePath = $_SERVER['DOCUMENT_ROOT'] . '/weblist-studio/uarconstruction'; //dirname(__FILE__);
+$codeVersion = date('Ymdhis');
 ?>
 
 <?php
 $userOsBrowser = $_SERVER['HTTP_USER_AGENT'];
 /////////////////////////////////////////////////////////////////////////////////
-function getUserIP() {
+function getUserIP()
+{
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         return $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -23,40 +24,41 @@ function getUserIP() {
         return $_SERVER['REMOTE_ADDR'];
     }
 }
-$userIpAddress =getUserIP();
+$userIpAddress = getUserIP();
 
 /////////////////////////////////////////////////////////////////////////////////
-function getBrowserId() {
+function getBrowserId()
+{
     $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';  // Browser and OS info
     $acceptLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '';  // Language
     // Combine all data and create a hash
     $browserId = hash('sha256', $userAgent . $acceptLanguage);
     return $browserId;
 }
-$userDeviceId=getBrowserId();
+$userDeviceId = getBrowserId();
 ?>
 
 <script>
-/// Constants ///
-var websiteUrl = "<?php echo $websiteUrl;?>";
-var userOsBrowser = "<?php echo $userOsBrowser;?>"; /// For User OS Browser //
-var userIpAddress = "<?php echo $userIpAddress;?>"; /// For User IP Address //
-var userDeviceId = "<?php echo $userDeviceId;?>"; /// For User Device Id //
+    /// Constants ///
+    var websiteUrl = "<?php echo $websiteUrl; ?>";
+    var userOsBrowser = "<?php echo $userOsBrowser; ?>"; /// For User OS Browser //
+    var userIpAddress = "<?php echo $userIpAddress; ?>"; /// For User IP Address //
+    var userDeviceId = "<?php echo $userDeviceId; ?>"; /// For User Device Id //
 
-/// Site Middleware Urls ///
-var siteMiddlewareUrl = websiteUrl + '/config/code'; //// For site url
+    /// Site Middleware Urls ///
+    var siteMiddlewareUrl = websiteUrl + '/config/code'; //// For site url
 
-/// Admin Middleware Urls ///
-var adminMiddleWareUrl = websiteUrl + '/admin/config/code'; /// For Admin Login Middleware Url //
-var adminUrl = websiteUrl + '/admin'; /// For Admin Url //
+    /// Admin Middleware Urls ///
+    var adminMiddleWareUrl = websiteUrl + '/admin/config/code'; /// For Admin Login Middleware Url //
+    var adminUrl = websiteUrl + '/admin'; /// For Admin Url //
 
-/// Admin Portal Middleware Urls ///
-var adminPortalMiddlewareUrl = websiteUrl + '/admin/portal/config/code'; /// For Admin Portal Middleware Url //
-var adminPortalUrl = websiteUrl + '/admin/portal'; /// For Portal Url //
+    /// Admin Portal Middleware Urls ///
+    var adminPortalMiddlewareUrl = websiteUrl + '/admin/portal/config/code'; /// For Admin Portal Middleware Url //
+    var adminPortalUrl = websiteUrl + '/admin/portal'; /// For Portal Url //
 
-var pageCategory = {
-    services: 'services',
-    portfolio: 'portfolio',
-    blog: 'blog'
-}
+    var pageCategory = {
+        services: 'services',
+        portfolio: 'portfolio',
+        blog: 'blog'
+    }
 </script>
--- meta.php
+++ meta.php
@@ -4,24 +4,26 @@
 <meta name="distribution" content="global">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link href="<?php echo $websiteUrl?>/all-images/images/icon.png" rel="shortcut icon" type="image-png"/>
-<link href="<?php echo $websiteUrl?>/style/icons-1.11.3/font/bootstrap-icons.css" type="text/css" rel="stylesheet" >
-<link href="<?php echo $websiteUrl?>/style/animate.css" type="text/css" rel="stylesheet" media="all">
-<link href="<?php echo $websiteUrl?>/style/aos.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo $websiteUrl?>/style/paramount.css?v=<?php echo $codeVersion?>" type="text/css" rel="stylesheet" />
-<link href="<?php echo $websiteUrl?>/style/main-style.css?v=<?php echo $codeVersion?>" type="text/css" rel="stylesheet" />
+<link href="<?php echo $websiteUrl ?>/all-images/images/icon.png" rel="shortcut icon" type="image-png" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+<link href="<?php echo $websiteUrl ?>/style/animate.css" type="text/css" rel="stylesheet" media="all">
+<link href="<?php echo $websiteUrl ?>/style/aos.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo $websiteUrl ?>/style/paramount.css?v=<?php echo $codeVersion ?>" type="text/css"
+    rel="stylesheet" />
+<link href="<?php echo $websiteUrl ?>/style/main-style.css?v=<?php echo $codeVersion ?>" type="text/css"
+    rel="stylesheet" />
 
-<script src="<?php echo $websiteUrl?>/js/jquery-v3.6.1.min.js"></script>
-<script src="<?php echo $websiteUrl?>/js/paramount.js?v=<?php echo $codeVersion?>"></script>
-<script src="<?php echo $websiteUrl?>/js/scripts.js?v=<?php echo $codeVersion?>"></script>
-<script src="<?php echo $websiteUrl?>/js/textfield-selectfield.js?v=<?php echo $codeVersion?>"></script>
-<script src="<?php echo $websiteUrl?>/js/aos.js"></script>
+<script src="<?php echo $websiteUrl ?>/js/jquery-v3.6.1.min.js"></script>
+<script src="<?php echo $websiteUrl ?>/js/paramount.js?v=<?php echo $codeVersion ?>"></script>
+<script src="<?php echo $websiteUrl ?>/js/scripts.js?v=<?php echo $codeVersion ?>"></script>
+<script src="<?php echo $websiteUrl ?>/js/textfield-selectfield.js?v=<?php echo $codeVersion ?>"></script>
+<script src="<?php echo $websiteUrl ?>/js/aos.js"></script>
 
-<link href="<?php echo $websiteUrl?>/slide-property/product-carousel/carousel.css" rel="stylesheet" type="text/css" />
-<script src="<?php echo $websiteUrl?>/slide-property/product-carousel/carousel.js" type="text/javascript" ></script>
+<link href="<?php echo $websiteUrl ?>/slide-property/product-carousel/carousel.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo $websiteUrl ?>/slide-property/product-carousel/carousel.js" type="text/javascript"></script>
 
-<link href="<?php echo $websiteUrl?>/slide-property/engine/style.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $websiteUrl ?>/slide-property/engine/style.css" rel="stylesheet" type="text/css" />
 
 <meta property="og:type" content="Website" />
-<meta property="og:site_name" content="<?php echo $appName?>">
-<meta property="og:url" content="<?php echo $websiteAutoUrl?>" />
+<meta property="og:site_name" content="<?php echo $appName ?>">
+<meta property="og:url" content="<?php echo $websiteAutoUrl ?>" />
