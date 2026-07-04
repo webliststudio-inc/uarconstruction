5e793c3dcfd87edde76d4da11a4e636ff98b6dfb
Emmytech12 <seunemmanuel107@gmail.com>
2026-07-04 22:44:28 +0100

    update: update 2 @03-07-2026 - update on portfolio

--- admin/portal/meta.php
+++ admin/portal/meta.php
@@ -41,3 +41,4 @@
 <script src="<?php echo $websiteUrl?>/admin/portal/js/useBlog.js?v=<?php echo $codeVersion?>" type="text/javascript"></script>
 <script src="<?php echo $websiteUrl?>/admin/portal/js/usePortfolio.js?v=<?php echo $codeVersion?>" type="text/javascript"></script>
 <script src="<?php echo $websiteUrl?>/admin/portal/js/useProjectCategory.js?v=<?php echo $codeVersion?>" type="text/javascript"></script>
+<script src="<?php echo $websiteUrl?>/admin/portal/js/useFaq.js?v=<?php echo $codeVersion?>" type="text/javascript"></script>
